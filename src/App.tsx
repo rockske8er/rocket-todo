6643d23e73a2039b20696548ff16bec53ed170ff
markus90souza <rockske8er@gmail.com>
2022-06-09 17:26:28 -0300

    feat: create input e button for create task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
 import { useState } from "react";
+import { PlusCircle } from "phosphor-react";
+import s from "./styles/app.module.css";
 import { Header } from "./components/Header";
 
 const App = () => {
   const [count, setCount] = useState(0);
 
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <main className={s.container}>
+        <div className={s.input__group}>
+          <input
+            className={s.input__add_todo}
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button className={s.btn__create_todo}>
+            Criar <PlusCircle width={16} />
+          </button>
+        </div>
+      </main>
+    </>
+  );
 };
 
 export { App };
